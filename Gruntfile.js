7ce7a05e7bafdaf2201b9177092fbd45eff4e2c5
yuanxin.qiu <yuanxin.qiu@smartlionapp.com>
2015-08-02 23:10:21 +0800

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+"use strict";
+
+module.exports = function(grunt) {
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    "babel": {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'dist/app.js': 'src/app.js'
+        }
+      }
+    }
+  });
+
+  grunt.registerTask("default", ["babel"]);
+
+};
